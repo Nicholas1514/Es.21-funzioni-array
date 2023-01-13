3e16419f9e64641d556214b18777a40540f79c87
Zappa.20201 <Zappa.20201@LABI2-13PC26>
2023-01-13 12:23:33 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es._21_ripasso_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]array = new int[100];
+            
+        }
+    }
+}
